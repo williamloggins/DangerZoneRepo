8c72b039d6cf66e83b0082f5d3b88b93e4db0946
williamloggins <88495431+williamloggins@users.noreply.github.com>
2021-11-29 19:27:35 -0500

    Initial ReverseString Posting

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word: ");
+            Console.WriteLine(ReverseArray(Console.ReadLine()));
+        }
+
+        public static string ReverseArray(string s)
+        {
+            char[] coolArray = s.ToCharArray();
+            Array.Reverse(coolArray);
+            return new string(coolArray);
+        }
+    }
+}
+
+// ask the user to enter a string
+// create a method that makes word a char array, then use the reverse function
+// call that method, then return that back to main to display
