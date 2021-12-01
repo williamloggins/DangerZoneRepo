36fe60e568e1d328f09f52559ecbabdb9f51a1cb
williamloggins <88495431+williamloggins@users.noreply.github.com>
2021-11-30 19:38:46 -0500

    Initial Calculator App

---
+++ BlakesCalculator/BlakesCalculator/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BlakesCalculator
+{
+    public static class Operations
+    {
+            public static double Addition(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+
+            public static double Subtraction(double num1, double num2)
+            {
+                return num1 - num2;
+            }
+
+            public static double Multiplication(double num1, double num2)
+            {
+                return num1 * num2;
+            }
+
+            public static double Division(double num1, double num2)
+            {
+                return num1 / num2;
+            }
+    }
+}
---
+++ BlakesCalculator/BlakesCalculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BlakesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0;
+            double num2 = 0;
+
+            IntroScene();
+            GetNumbers(ref num1, ref num2);
+            AskOperation(ref num1, ref num2);
+            ExitScene();
+            Console.ReadLine();
+        }
+
+        public static void IntroScene()
+        {
+            Console.WriteLine("Welcome to Blake's definitely unique Calculator App!");
+        }
+
+        public static void GetNumbers(ref double num1, ref double num2)
+        {
+            Console.Write("\nPlease enter a number: ");
+
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nPlease enter a second number: ");
+
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void AskOperation(ref double num1, ref double num2)
+        {
+            Console.WriteLine("\nWhat kind of operation would you like to do?\n");
+            Console.WriteLine("A - Addition");
+            Console.WriteLine("S - Subtraction");
+            Console.WriteLine("M - Multiplication");
+            Console.WriteLine("D - Division");
+            Console.Write("\nOperation choice: ");
+            string response = Console.ReadLine().ToUpper();
+            double answer;
+
+            switch (response)
+            {
+                case "A":
+                    answer = Operations.Addition(num1, num2);
+                    Console.WriteLine($"\nYour result: {num1} + {num2} = {answer}");
+                    break;
+
+                case "S":
+                    answer = Operations.Subtraction(num1, num2);
+                    Console.WriteLine($"\nYour result: {num1} - {num2} = {answer}");
+                    break;
+
+                case "M":
+                    answer = Operations.Multiplication(num1, num2);
+                    Console.WriteLine($"\nYour result: {num1} x {num2} = {answer}");
+                    break;
+
+                case "D":
+                    answer = Operations.Division(num1, num2);
+                    Console.WriteLine($"\nYour result: {num1} / {num2} = {answer}");
+                    break;
+
+                default:
+                    Console.WriteLine("\nHey! That's not one of the choices.");
+                    break;
+            }
+        }
+
+        public static void ExitScene()
+        {
+            Console.WriteLine("\nThanks so much for using Blake's definitely unique Calculator App! 😊");
+        }
+    }
+}
